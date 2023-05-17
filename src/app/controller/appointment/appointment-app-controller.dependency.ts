2a3619eb1080da1cc723514091a4442278be365d
LuxyKolink <santiagolikeaboss22@gmail.com>
2023-05-17 17:45:41 -0500

    app models controllers + web app adapters

---
+++ src/app/controller/appointment/appointment-app-controller.dependency.ts
@@ -0,0 +1,9 @@
+import Appointment from "../../../domain/appointment.domain";
+import AppointmentAppControllerPort from "./appointment-app-controller.port";
+import AppointmentAppModelPort from "../../model/appointment/appointment-app-model.port";
+
+export {
+    Appointment,
+    AppointmentAppControllerPort,
+    AppointmentAppModelPort
+}
---
+++ src/web-app/adapter/appointment/appointment-web-app-adapter.dependency.ts
@@ -0,0 +1,9 @@
+import Appointment from "../../../domain/appointment.domain";
+import AppointmentAppControllerPort from "../../../app/controller/appointment/appointment-app-controller.port";
+import AppointmentWebAppAdapterPort from "./appointment-web-app-adapter.port";
+
+export {
+    Appointment,
+    AppointmentAppControllerPort,
+    AppointmentWebAppAdapterPort
+}
